22ee9a63a20e66212bf88a596f910e206cb9a387
uridev <uriidesigner@gmail.com>
2025-07-29 23:55:43 -0300

    add if_else

---
+++ if_else/if_else.c
@@ -0,0 +1,20 @@
+
+
+#include <stdio.h>
+
+int main()
+{
+  int num1 = 10;
+  int num2 = 10;
+
+  if (num1 != num2)
+  {
+    printf("O num1 é diferente de num2");
+  }
+  else
+  {
+    printf("Os num são iguais");
+  }
+
+  return 0;
+}
